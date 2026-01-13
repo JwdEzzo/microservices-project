e3de21a5928a35206620bd162063b12d2f4cd00e
Jawad Ezzo <ezzjawad@hotmail.com>
2026-01-13 16:31:22 +0200

    first commit

---
+++ school-microservice/src/test/java/com/school/school_microservice/SchoolMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.school_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
